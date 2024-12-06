c7ea1f0ce9e85a2b30b4362fe477393ae708b15e
benny <cs921029@github.com>
2024-12-06 11:48:41 +0800

    [Feature] git init

---
+++ src/main/java/com/example/DailyBot/DailyBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.DailyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DailyBot/DailyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DailyBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
